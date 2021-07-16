38aceca3099dc3f7219913c69d4546a0d033781e
brandondb1 <54447239+brandondb1@users.noreply.github.com>
2021-07-16 11:37:53 -0400

    update

--- custom_components/virage_dashboard/load_plugins.py
+++ custom_components/virage_dashboard/load_plugins.py
@@ -6,25 +6,18 @@ DATA_EXTRA_MODULE_URL = 'frontend_extra_module_url'
 _LOGGER = logging.getLogger(__name__)
 
 def load_plugins(hass, name):
- #   add_extra_js_url(hass, "/dwains_dashboard/js/dwains-dashboard.js")
 
-    #Cards by others
-    #add_extra_js_url(hass, "/virage_dashboard/cards/button-card/button-card.js")
-    #add_extra_js_url(hass, "/virage_dashboard/cards/light-entity-card/light-entity-card.js")
     add_extra_js_url(hass, "/virage_dashboard/cards/auto-entities/auto-entities.js")
     add_extra_js_url(hass, "/virage_dashboard/cards/vertical-stack-in-card/vertical-stack-in-card.js")
-
-    #Cards by dwains
-  #  add_extra_js_url(hass, "/dwains_dashboard/cards/dwains-header-card/dwains-header-card.js")
-  #  add_extra_js_url(hass, "/dwains_dashboard/cards/dwains-heading-card/dwains-heading-card.js")
-  #  add_extra_js_url(hass, "/dwains_dashboard/cards/dwains-wrapper-card/dwains-wrapper-card.js")
-  #  add_extra_js_url(hass, "/dwains_dashboard/cards/dwains-flexbox-card/dwains-flexbox-card.js")
-  #  add_extra_js_url(hass, "/dwains_dashboard/cards/dwains-hash-switch-card/dwains-hash-switch-card.js")
-  #  add_extra_js_url(hass, "/dwains_dashboard/cards/dwains-weather-card/dwains-weather-card.js")
-  #  add_extra_js_url(hass, "/dwains_dashboard/cards/dwains-notification-card/dwains-notification-card.js")
-  #  add_extra_js_url(hass, "/dwains_dashboard/cards/dwains-collapse-card/dwains-collapse-card.js")
-  #  add_extra_js_url(hass, "/dwains_dashboard/cards/dwains-cover-card/dwains-cover-card.js")
-  #  add_extra_js_url(hass, "/dwains_dashboard/cards/dwains-auto-entities-card/dwains-auto-entities-card.js")
-
-   # hass.http.register_static_path("/dwains_dashboard/js", hass.config.path(f"custom_components/{name}/js"), True)
     hass.http.register_static_path("/virage_dashboard/cards", hass.config.path(f"custom_components/{name}/cards"), True)
+    
+       for fname in os.listdir(hass.config.path("custom_components/virage_dashboard/blueprints")):
+        if not os.path.isfile(hass.config.path("blueprints/automation/viragelabs/"+fname)):
+            if fname.endswith('.yaml'):
+                _LOGGER.debug("Copy: "+fname)
+                os.makedirs(hass.config.path("blueprints/automation/viragelabs"), exist_ok=True)
+                shutil.copy2(
+                    hass.config.path("custom_components/virage_dashboard/blueprints/"+fname),
+                    hass.config.path("blueprints/automation/viragelabs")
+                    
+            )
